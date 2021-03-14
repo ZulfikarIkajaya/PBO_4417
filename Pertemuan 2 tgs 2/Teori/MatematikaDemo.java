3a7e7d0b060c3d13daafdf0c4ec3feb44b825f12
ZulfikarIkajaya <80262425+ZulfikarIkajaya@users.noreply.github.com>
2021-03-14 21:38:16 +0700

    Add files via upload

---
+++ Pertemuan 2 tgs 2/Teori/MatematikaDemo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Matematika {
+    int a;
+    int b;
+    int c;
+    int d;
+    double tambah, kurang, kali,bagi;
+}
+public class MatematikaDemo {
+    public static void main(String[] args){
+        int a = 15;
+        int b = 10;
+        int c = 8;
+        int d = 3;
+
+        int tambah=a+a;
+        int kurang=b-c;
+        int kali=a*b;
+        int bagi=a/d;
+
+        System.out.println("tambah :"+tambah);
+        System.out.println("kurang :"+kurang);
+        System.out.println("kali :"+kali);
+        System.out.println("bagi :"+bagi); 
+    }
+}
---
+++ Pertemuan 2 tgs 2/Teori/MobilDemo2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Mobil2 {
+    String warna;
+    int tahunProduksi;
+    boolean hidupkanMobil = false;
+    boolean matikanMobil = true;
+    int ubahGigi;
+}
+public class MobilDemo2 {
+    public static void main(String[] args){
+        Mobil mobilku = new Mobil();
+        Scanner keyboard = new Scanner(System.in);
+        
+        
+        System.out.print(" Masukkan Warna :");
+        mobilku.warna = keyboard.nextLine();
+        System.out.print(" Masukkan Tahun :");
+        mobilku.tahunProduksi = keyboard.nextInt();
+        System.out.println(" Mesin :"+ mobilku.hidupkanMobil);
+        System.out.println(" Ubah Gigi:"+ mobilku.ubahGigi);
+        mobilku.ubahGigi = 2;
+        System.out.println(" Press any key to continue... ");
+    }
+}
