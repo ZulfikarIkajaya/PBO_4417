e4c8b73620364f4344883e8a40c7d38bf443d9de
ZulfikarIkajaya <80262425+ZulfikarIkajaya@users.noreply.github.com>
2021-03-23 10:22:25 +0700

    Add files via upload

---
+++ Pertemuan3/TestApp.java
@@ -0,0 +1,10 @@
+public class TestApp {
+    public static void main(String[] args){
+    gameplayer mtk = new gameplayer();
+    gameEnemy rui = new gameEnemy();
+    mtk.run();
+    mtk.Run(30);
+    rui.ruun();
+
+    }
+} 
---
+++ Pertemuan3/gameEnemy.java
@@ -0,0 +1,57 @@
+public class gameEnemy{
+    int PositionX ;
+    int PositionY;
+    double width;
+    double height;
+    int ruun;
+
+    gameEnemy(){
+
+    }
+
+
+    gameEnemy(double width, double height){
+
+    }
+
+
+    gameEnemy (double width, double height, int PositionX, int PositionY){
+
+
+    }
+
+    public void setDimensi(double width, double height){
+
+    }
+
+    public void setPosition(int PositionX, int PositionY){
+
+    }
+
+    public double getWidth(){
+        return 10.0;
+    }
+
+    public double getHeight(){
+        return 10.0;
+    }
+
+    public int getX(){
+        return 10;
+    }
+
+    public int getY(){
+        return 10;
+    }
+
+    public void ruun(){
+        System.out.println(" \n Enemy is Running");
+    }
+
+}
+
+
+
+
+
+
