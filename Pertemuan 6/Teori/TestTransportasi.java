7c312aa4f3e11eb28e3d22de62adf1c230d8f782
ZulfikarIkajaya <80262425+ZulfikarIkajaya@users.noreply.github.com>
2021-04-11 21:06:00 +0700

    Add files via upload

---
+++ Pertemuan 6/Teori/TestTransportasi.java
@@ -0,0 +1,14 @@
+import Transportasi.TestSepeda;
+import Transportasi.MobilTest;
+
+public class TestTransportasi{
+    public static void main(String[] args){
+        TestSepeda Jaw = new TestSepeda();
+        System.out.println("====== Sepeda BM ====== ");
+        Jaw.JalanKanan();
+        Jaw.JalanLurus();
+        MobilTest Lal = new MobilTest();
+        System.out.println("====== Menu Mobil ====== ");
+        Lal.Menu();
+    }
+}
